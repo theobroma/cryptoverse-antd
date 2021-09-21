1338bea830c1a486c96ae5f9699ad0385cc41c50
Alexandr Siryj <theobroma222@gmail.com>
2021-09-21 23:51:59 +0300

    add libs

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import './table.css';
-import BasicTable from './@components/BasicTable';
 
 function App() {
   return (
     <div className="App">
-      <BasicTable />
+      <span>123</span>
     </div>
   );
 }
