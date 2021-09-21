18ea6f2e9669232bb006de4ff6220148038bb52e
Alexandr Siryj <theobroma222@gmail.com>
2021-09-22 00:30:50 +0300

    configureStore

---
+++ src/@store/configureStore.ts
@@ -0,0 +1,18 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { createLogger } from 'redux-logger';
+
+const logger = createLogger({
+  collapsed: true,
+});
+
+export const store = configureStore({
+  reducer: {
+    // [cryptoApi.reducerPath]: cryptoApi.reducer,
+    // [cryptoNewsApi.reducerPath]: cryptoNewsApi.reducer,
+  },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+  // devTools: process.env.NODE_ENV !== 'production',
+  devTools: true,
+});
+
+export default { store };
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import 'antd/dist/antd.css';
+import { store } from './@store/configureStore';
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root'),
 );
