2e39359195c8cb60c4dc8ffa5ddb7312a66bd780
Alexandr Siryj <theobroma222@gmail.com>
2021-09-22 17:30:12 +0300

    basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
+import { Switch, Route, Link } from 'react-router-dom';
+import { Layout, Typography, Space } from 'antd';
+import { Navbar } from './@components';
 
-function App() {
-  return (
-    <div className="App">
-      <span>123</span>
-    </div>
-  );
-}
+// import { Exchanges, Homepage, News, Cryptocurrencies, CryptoDetails, Navbar } from './components';
+// import './App.css';
+const { Header, Footer, Sider, Content } = Layout;
+
+const App = () => (
+  <div className="app">
+    <Layout>
+      <Sider>Sider</Sider>
+      <Layout>
+        <Header>Header</Header>
+        <Content>Content</Content>
+        <Footer>Footer</Footer>
+      </Layout>
+    </Layout>
+  </div>
+);
 
 export default App;
